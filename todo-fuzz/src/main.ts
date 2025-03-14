3dfee0d3b6fda03b2a947b282a0b1e3a8f5f85a6
dtap <tapai.david@cae.com>
2025-03-14 22:16:42 +0100

    feat: bootstrap

---
+++ todo-fuzz/src/main.ts
@@ -0,0 +1,7 @@
+import  red  from 'chalk';
+import signale from "signale"
+
+console.log(red("lofasz"));
+
+signale.success("wtf is this");
+// https://github.com/klaudiosinani/taskbook/blob/master/src/render.js
---
+++ todo-fuzz/src/utils/user-interactor.utility.ts
@@ -0,0 +1,38 @@
+import prompts from 'prompts';
+import { Fzf } from 'fzf';
+import promptSync from 'prompt-sync';
+
+export class UserInteractor {
+    static async selectFromList(
+      question: string,
+      list: string[]
+    ): Promise<string> {
+      const choices = list.map((item: string) => {
+        return { title: item, value: item } as prompts.Choice;
+      });
+  
+      const fzf = new Fzf(list);
+      const result = await prompts({
+        type: 'autocomplete',
+        name: 'value',
+        message: question,
+        choices: choices,
+        suggest: (input: string) =>
+          Promise.resolve(fzf.find(input).map((item) => item.item)),
+      });
+      return result.value;
+    }
+  
+    static prompt(
+      question: string,
+      validation: (input: string) => boolean
+    ): string {
+      let result;
+      do {
+        const promptInstance = promptSync({ sigint: true });
+        result = promptInstance(question);
+      } while (!validation(result));
+      return result;
+    }
+  }
+  
